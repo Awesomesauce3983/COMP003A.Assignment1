1173e41afc0aae04fe14f0d0613eed30dd4790d6
willi <willi@DESKTOP-M9HF8K8>
2023-01-20 19:04:25 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,114 @@
+﻿/*  Author: Anastasia Clements
+ *  Course: COMP-003A-L01
+ * Purpose: 100 Things about Me!
+*/
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things about Me:");
+            Console.WriteLine("1. My name is Ana");
+            Console.WriteLine("2. I am 19 years old");
+            Console.WriteLine("3. I live in Hanford California");
+            Console.WriteLine("4. My favorite color is green");
+            Console.WriteLine("5. I like lots of games");
+            Console.WriteLine("6. One of my favorites is Stardew Valley");
+            Console.WriteLine("7. On Steam though, my top game is actually Terraria");
+            Console.WriteLine("8. I primarily play games on my computer");
+            Console.WriteLine("9. I've been playing computer games since I was a little kid");
+            Console.WriteLine("10. My gaming interests have changed over the years");
+            Console.WriteLine("11. But sometimes those games I liked as a kid still have their charms");
+            Console.WriteLine("12. Another game I like is a tabletop game, D&D");
+            Console.WriteLine("13. I've only every played Fifth Edition of D&D (5e)");
+            Console.WriteLine("14. I've played a bunch of characters over the years.");
+            Console.WriteLine("15. What was fun was completing a 1.5 year campaign");
+            Console.WriteLine("16. Our party name was 'The Wayfaring Strangers'");
+            Console.WriteLine("17. I played a Hexblade Warlock for that game, it was super fun!");
+            Console.WriteLine("18. Our playgroup started a new campaign after the conclusion of the last one");
+            Console.WriteLine("19. Our new party name is Vox Arcanum (yeah I know inspired by Vox Machina)");
+            Console.WriteLine("20. In this new game I play a Order of Scribes Wizard");
+            Console.WriteLine("21. This new game has gone on for longer than the old one");
+            Console.WriteLine("22. Speaking of which, if a 'thing' pertains to me it satisfies the technical requirements of this assignment");
+            Console.WriteLine("23. From this thought process, I will spend my remaining time recounting 77 terms of the Fibbonaci Sequence, since It is my favorite sequence of numbers");
+            Console.WriteLine("24. 0");
+            Console.WriteLine("25. 1");
+            Console.WriteLine("26. 1");
+            Console.WriteLine("27. 2");
+            Console.WriteLine("28. 3");
+            Console.WriteLine("29. 5");
+            Console.WriteLine("30. 8");
+            Console.WriteLine("31. 13");
+            Console.WriteLine("32. 21");
+            Console.WriteLine("33. 34");
+            Console.WriteLine("34. 55");
+            Console.WriteLine("35. 89");
+            Console.WriteLine("36. 144");
+            Console.WriteLine("37. 233");
+            Console.WriteLine("38. 377");
+            Console.WriteLine("39. 610");
+            Console.WriteLine("40. 987");
+            Console.WriteLine("41. 1597");
+            Console.WriteLine("42. 2584");
+            Console.WriteLine("43. 4181");
+            Console.WriteLine("44. 6765");
+            Console.WriteLine("45. 10946");
+            Console.WriteLine("46. 17711");
+            Console.WriteLine("47. 28657");
+            Console.WriteLine("48. 46368");
+            Console.WriteLine("49. 75025");
+            Console.WriteLine("50. 121393");
+            Console.WriteLine("51. 196418");
+            Console.WriteLine("52. 317811");
+            Console.WriteLine("53. 514229");
+            Console.WriteLine("54. 832040");
+            Console.WriteLine("55. 1346269");
+            Console.WriteLine("56. 2178309");
+            Console.WriteLine("57. 3524578");
+            Console.WriteLine("58. 5702887");
+            Console.WriteLine("59. 9227465");
+            Console.WriteLine("60. 14930352");
+            Console.WriteLine("61. 24157817");
+            Console.WriteLine("62. 39088169");
+            Console.WriteLine("63. 63245986");
+            Console.WriteLine("64. 102334155");
+            Console.WriteLine("65. 165580141");
+            Console.WriteLine("66. 267914296");
+            Console.WriteLine("67. 433494437");
+            Console.WriteLine("68. 701408733");
+            Console.WriteLine("69. 1134903170");
+            Console.WriteLine("70. 1836311903");
+            Console.WriteLine("71. 2971215073");
+            Console.WriteLine("72. 4807526976");
+            Console.WriteLine("73. 7778742049");
+            Console.WriteLine("74. 12586269025");
+            Console.WriteLine("75. 20365011074");
+            Console.WriteLine("76. 32951280099");
+            Console.WriteLine("77. 53316291173");
+            Console.WriteLine("78. 86267571272");
+            Console.WriteLine("79. 139583862445");
+            Console.WriteLine("80. 225851433717");
+            Console.WriteLine("81. 365435296162");
+            Console.WriteLine("82. 591286729879");
+            Console.WriteLine("83. 956722026041");
+            Console.WriteLine("84. 1548008755920");
+            Console.WriteLine("85. 2504730781961");
+            Console.WriteLine("86. 4052739537881");
+            Console.WriteLine("87. 6557470319842");
+            Console.WriteLine("88. 10610209857723");
+            Console.WriteLine("89. 17167680177565");
+            Console.WriteLine("90. 27777890035288");
+            Console.WriteLine("91. 44945570212853");
+            Console.WriteLine("92. 72723460248141");
+            Console.WriteLine("93. 117669030460994");
+            Console.WriteLine("94. 190392490709135");
+            Console.WriteLine("95. 308061521170129");
+            Console.WriteLine("96. 498454011879264");
+            Console.WriteLine("97. 806515533049393");
+            Console.WriteLine("98. 1304969544928657");
+            Console.WriteLine("99. 2111485077978050");
+            Console.WriteLine("100. 3416454622906707");
+        }
+    }
+}
